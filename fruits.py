43516249982f17f436f669980aebcbe21ab077ef
nagendranambu22-dotcom <nagendranambu22@gmail.com>
2026-09-18 17:33:17 +0530

    add nwe files

---
+++ fruits.py
@@ -0,0 +1,5 @@
+fruits=("appple", "banana", "orange")
+print(fruits)
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
---
+++ swapping.py
@@ -0,0 +1,9 @@
+a=34
+b=76
+print("befote swapping")
+print("a= ",a)
+print("b= ",b)
+a,b=b,a
+print("after swapping")
+print("a= ",a)
+print("b= ",b)
